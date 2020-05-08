798fbaa6b174ddd5c71f77b3cb009642555f5cb7
kkornel <kornel.krzeslak@student.put.poznan.pl>
2020-05-08 17:56:32 +0200

    Integrated pug engine.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 require('dotenv').config();
 require('./db/mongoose');
@@ -6,6 +7,16 @@ require('./db/mongoose');
 const authRouter = require('./routers/authRouter');
 
 const app = express();
+
+// Paths for Express config
+const publicDirectory = path.join(__dirname, '../public');
+const viewsDirectory = path.join(__dirname, '../templates/views');
+
+// Set the pug as a view engine
+app.set('view engine', 'pug');
+
+// Set the directory where the template files are located
+app.set('views', viewsDirectory);
 
 app.use(express.json());
 app.use(authRouter);
