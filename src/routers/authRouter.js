018fcbd4706853d474f584e67044b56a9804805d
kkornel <kornel.krzeslak@student.put.poznan.pl>
2020-05-09 16:55:26 +0200

    Unified route handlers.

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -8,51 +8,47 @@ const config = require('../config/config');
 
 const router = new Router();
 
-// TODO: Unify route handling
-// TODO: Resend token if expired.
-// TODO: Delete tokens
-
 router.post('/api/auth/register', async (req, res) => {
   console.log('register', req.body);
-  const { email, username, password } = req.body;
-
-  const emailTaken = await User.findOne({ email });
-
-  if (emailTaken) {
-    // Google uses 409 for ALREADY_EXISTS:
-    // The resource that a client tried to create already exists.
-    return res.status(409).send({
-      error: {
-        code: 409,
-        status: 'ALREADY_EXISTS',
-        message: 'Email already in use.',
-        details: {
-          field: 'email',
-          value: email,
-        },
-      },
-    });
-  }
-
-  const usernameTaken = await User.findOne({ username });
-
-  if (usernameTaken) {
-    return res.status(409).send({
-      error: {
-        code: 409,
-        status: 'ALREADY_EXISTS',
-        message: 'Username already in use.',
-        details: {
-          field: 'username',
-          value: username,
-        },
-      },
-    });
-  }
-
-  const user = new User({ email, username, password });
-
   try {
+    const { email, username, password } = req.body;
+
+    const emailTaken = await User.findOne({ email });
+
+    if (emailTaken) {
+      // Google uses 409 for ALREADY_EXISTS:
+      // The resource that a client tried to create already exists.
+      return res.status(409).send({
+        error: {
+          code: 409,
+          status: 'ALREADY_EXISTS',
+          message: 'Email already in use.',
+          details: {
+            field: 'email',
+            value: email,
+          },
+        },
+      });
+    }
+
+    const usernameTaken = await User.findOne({ username });
+
+    if (usernameTaken) {
+      return res.status(409).send({
+        error: {
+          code: 409,
+          status: 'ALREADY_EXISTS',
+          message: 'Username already in use.',
+          details: {
+            field: 'username',
+            value: username,
+          },
+        },
+      });
+    }
+
+    const user = new User({ email, username, password });
+
     await user.save();
 
     const token = user.generateToken(config.verificationTokenExpireTime);
@@ -75,71 +71,11 @@ router.post('/api/auth/register', async (req, res) => {
   }
 });
 
-router.get('/api/auth/verify/:token', async (req, res) => {
-  console.log('verify', req.body);
-  if (!req.params.token) {
-    // 400 INVALID_ARGUMENT
-    // Client specified an invalid argument. Check error message and error details for more information.
-    return res.status(400).send({
-      error: {
-        code: 400,
-        status: 'INVALID_ARGUMENT',
-        message: 'No token provided.',
-      },
-    });
-  }
-
-  try {
-    const token = await Token.findOne({ token: req.params.token });
-
-    if (!token) {
-      return res.status(400).send({
-        error: {
-          code: 400,
-          status: 'INVALID_ARGUMENT',
-          message: 'Invalid token.',
-        },
-      });
-    }
-
-    // const elapsedSeconds = (Date.now() - token.createdAt.getTime()) / 1000;
-    // const expired = elapsedSeconds > token.expiresIn;
-
-    const expired = Date.now() > token.expires;
-
-    if (expired) {
-      res.status(400).send({
-        error: {
-          code: 400,
-          status: 'BAD_REQUEST',
-          message: 'Token expired.',
-        },
-      });
-    }
-
-    await User.findByIdAndUpdate(token.userId, { active: true });
-    await token.delete();
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).send({
-      error: {
-        code: 500,
-        status: 'INTERNAL_SERVER_ERROR',
-      },
-    });
-  }
-
-  res.status(200).send({
-    message: 'The account has been verified.',
-  });
-});
-
 router.post('/api/auth/login', async (req, res) => {
   console.log('login', req.body);
-  const { email, password } = req.body;
-
   try {
+    const { email, password } = req.body;
+
     const user = await User.findByCredentials(email, password);
 
     if (!user.active) {
@@ -176,30 +112,40 @@ router.post('/api/auth/login', async (req, res) => {
   }
 });
 
-router.post('/api/auth/password/reset', async (req, res) => {
-  console.log('reset', req.body);
+router.get('/api/auth/verify/:token', async (req, res) => {
+  console.log('verify/:token', req.body);
   try {
-    const { email } = req.body;
+    const token = await Token.findOne({ token: req.params.token });
 
-    const user = await User.findOne({ email });
-
-    if (!user) {
+    if (!token) {
       return res.status(400).send({
         error: {
           code: 400,
-          status: 'BAD_REQUEST',
-          message: `The email address ${email} is not associated with any account.`,
+          status: 'INVALID_ARGUMENT',
+          message: 'Invalid token.',
         },
       });
     }
 
-    const token = user.generateToken(config.verificationTokenExpireTime);
-    await token.save();
+    const expired = Date.now() > token.expires;
 
-    user.sendPasswordResetEmail(token.token);
+    if (expired) {
+      await token.delete();
+
+      return res.status(400).send({
+        error: {
+          code: 400,
+          status: 'BAD_REQUEST',
+          message: 'Token expired.',
+        },
+      });
+    }
+
+    await User.findByIdAndUpdate(token.userId, { active: true });
+    await token.delete();
 
     res.status(200).send({
-      message: `A reset email has been sent to ${user.email}`,
+      message: 'The account has been verified.',
     });
   } catch (error) {
     console.log(error);
@@ -214,6 +160,7 @@ router.post('/api/auth/password/reset', async (req, res) => {
 });
 
 router.post('/api/auth/verify', async (req, res) => {
+  console.log('verify/', req.body);
   try {
     const { email } = req.body;
 
@@ -250,6 +197,7 @@ router.post('/api/auth/verify', async (req, res) => {
 });
 
 router.get('/api/auth/password/reset/:token', async (req, res) => {
+  console.log('reset/:token', req.body);
   try {
     // Note: If here would be the validation after navigating from email link
     // the user would get raw JSON in the browser with the error.
@@ -262,27 +210,29 @@ router.get('/api/auth/password/reset/:token', async (req, res) => {
 
     const token = await Token.findOne({ token: req.params.token });
 
-    // if (!token) {
-    //   return res.status(400).send({
-    //     error: {
-    //       code: 400,
-    //       status: 'INVALID_ARGUMENT',
-    //       message: 'Invalid token.',
-    //     },
-    //   });
-    // }
+    if (!token) {
+      return res.status(400).send({
+        error: {
+          code: 400,
+          status: 'INVALID_ARGUMENT',
+          message: 'Invalid token.',
+        },
+      });
+    }
 
-    // const expired = Date.now() > token.expires;
+    const expired = Date.now() > token.expires;
 
-    // if (expired) {
-    //   return res.status(400).send({
-    //     error: {
-    //       code: 400,
-    //       status: 'BAD_REQUEST',
-    //       message: 'Token expired.',
-    //     },
-    //   });
-    // }
+    if (expired) {
+      await token.delete();
+
+      return res.status(400).send({
+        error: {
+          code: 400,
+          status: 'BAD_REQUEST',
+          message: 'Token expired.',
+        },
+      });
+    }
 
     res.cookie('token', token.token, { httpOnly: true });
     res.redirect('/password/reset/new');
@@ -298,7 +248,45 @@ router.get('/api/auth/password/reset/:token', async (req, res) => {
   }
 });
 
+router.post('/api/auth/password/reset', async (req, res) => {
+  console.log('reset', req.body);
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send({
+        error: {
+          code: 400,
+          status: 'BAD_REQUEST',
+          message: `The email address ${email} is not associated with any account.`,
+        },
+      });
+    }
+
+    const token = user.generateToken(config.passwordResetTokenExpireTime);
+    await token.save();
+
+    user.sendPasswordResetEmail(token.token);
+
+    res.status(200).send({
+      message: `A reset email has been sent to ${user.email}`,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      error: {
+        code: 500,
+        status: 'INTERNAL_SERVER_ERROR',
+      },
+    });
+  }
+});
+
 router.post('/api/auth/password/reset/new', async (req, res) => {
+  console.log('reset/new', req.body);
   console.log('cookies', req.cookies);
   try {
     const token = await Token.findOne({ token: req.cookies.token });
@@ -316,6 +304,8 @@ router.post('/api/auth/password/reset/new', async (req, res) => {
     const expired = Date.now() > token.expires;
 
     if (expired) {
+      await token.delete();
+
       return res.status(400).send({
         error: {
           code: 400,
