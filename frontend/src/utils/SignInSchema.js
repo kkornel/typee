fd7f6ce67a361f24c7c733361e7f37cb97a08f0f
kkornel <kornel.krzeslak@student.put.poznan.pl>
2020-05-23 10:57:25 +0200

    Cleaned up Auth components.

--- frontend/src/utils/SignInSchema.js
+++ frontend/src/utils/SignInSchema.js
@@ -1,7 +1,5 @@
 import * as yup from 'yup';
 
-import passwordValidator from './passwordValidator';
-
 const SignInSchema = yup.object().shape({
   email: yup.string().required('Required.').email('Invalid email.'),
   password: yup.string().required('Required.'),
