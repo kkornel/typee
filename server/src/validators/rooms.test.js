cff2300d85a33adc870a3c1d6fc8e4a29a21c919
Kornel <krzeslak.kornel@gmail.com>
2020-09-30 10:03:00 +0200

    Added tests for Room Validator

--- server/src/validators/rooms.test.js
+++ server/src/validators/rooms.test.js
@@ -1,1 +1,40 @@
-// TODO
+const request = require('supertest');
+
+const app = require('../app');
+
+const User = require('../models/User');
+
+const {
+  setupDatabase,
+  clearDatabase,
+  userOneId,
+  roomOneName,
+} = require('../../tests/fixtures/db');
+
+beforeEach(setupDatabase);
+afterEach(clearDatabase);
+
+describe('room validator', () => {
+  it('should respond with 422 for invalid new name', async () => {
+    const user = await User.findById(userOneId);
+    const token = user.jwtTokens[0];
+
+    const response = await request(app)
+      .post(`/api/v1/rooms/${roomOneName}`)
+      .set('Authorization', `Bearer ${token.token}`)
+      .send({
+        deleteAvatar: 'false',
+        newName: 'ToooooooooooooooooooooooooooooooooooLong',
+      })
+      .expect('Content-Type', /json/)
+      .expect(422);
+
+    const errors = {
+      newName: {
+        message: 'Max length is 25',
+      },
+    };
+
+    expect(response.body).toMatchObject({ errors });
+  });
+});
